fca8ad82225655c133bb60d91bd8774a1623ae19
ben3100 <131266856+ben3100@users.noreply.github.com>
2026-02-05 23:21:11 +0100

    Add Home component for TikTok Info API

---
+++ index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "Arial", padding: 30 }}>
+      <h1>✅ TikTok Info API</h1>
+      <p>Test :</p>
+      <code>/api/tiktokinfo?username=khaby</code>
+    </main>
+  );
+}
